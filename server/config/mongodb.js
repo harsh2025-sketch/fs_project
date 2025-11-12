d4e13882ee8be79f314e546e0f782ca34bab7713
harsh2025-sketch <ghosttalk719@gmail.com>
2025-11-12 20:08:41 +0530

    first commit

--- server/config/mongodb.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
- 
-const connectDB = async()=>{
-   mongoose.connection.on('Connected',()=>console.log("Database 👌 Connected"));
-   await mongoose.connect(`${process.env.MONGODB_URL}mern-auth`)
- }
-
- export default connectDB;
